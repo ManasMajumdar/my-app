eb31e733a4cf749b2db94cee437b2e2b8d380978
ManasMajumdar <manasmajumdar889@gmail.com>
2023-09-04 17:53:24 +0530

    Adding Bootstrap

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ function App() {
   return (
    <>
 
-   <nav>
-    <li>Home</li>
-    <li>About</li>
-    <li>Contact</li>
-   </nav>
-   <div className="container">
-    <h1>Hello {name}</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, ducimus.</p>
-   </div>
+   
 
    </>
   );
