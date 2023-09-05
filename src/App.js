93d608565a79c0bc840818acdb31c1217a6c0d79
ManasMajumdar <manasmajumdar889@gmail.com>
2023-09-05 18:23:32 +0530

    About

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
+// import TextForm from './Components/TextForm';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
    {/* <Navbar title = "MyApp2" /> - if we send one thing than other things its takes from default props*/}
    {/* <Navbar/> - if we want to pass default propTypes */}
    <div className="container">
-   <TextForm heading="Enter the text to analyze below" />
+   {/* <TextForm heading="Enter the text to analyze below" /> */}
+   <About/>
    </div>
    </>
   );
